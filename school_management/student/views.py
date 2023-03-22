b18a786d8fd1ec2386e5a1b172d97c8d975a9e3b
Benjamin569 <54053468+Benjamin569@users.noreply.github.com>
2023-03-22 02:03:44 +0000

    register

--- school_management/student/views.py
+++ school_management/student/views.py
@@ -6,3 +6,12 @@ from django.template import loader
 def index(request):
     template = loader.get_template('index.html')
     return HttpResponse (template.render())
+
+
+def signin(request):
+    template = loader.get_template('signin.html')
+    return HttpResponse(template.render())
+
+def signup(request):
+    template = loader.get_template('signup.html')
+    return HttpResponse(template.render())
