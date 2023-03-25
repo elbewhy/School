4311e98c04230e7abfb676bb28618989a209ffe3
Benjamin569 <54053468+Benjamin569@users.noreply.github.com>
2023-03-25 17:01:01 +0000

    done backend for log in

--- school_management/student/models.py
+++ school_management/student/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth import get_user_model
 
 user = get_user_model()
 
-class Profile (models.Model):
+class Profile(models.Model):
   user = models.ForeignKey(user, on_delete=models.CASCADE)
   id_user = models.IntegerField()
   bio = models.TextField(blank=True)
